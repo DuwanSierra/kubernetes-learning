3b17ba0316851c4655d9e451703709436b94702e
Duwan Sierra <desierrag@gmail.com>
2022-05-21 18:28:17 -0500

    add other docker install

--- install-dns-and-docker-ubuntu.sh
+++ install-dns-and-docker-ubuntu.sh
@@ -5,11 +5,13 @@
 sudo apt-get update -y
 sudo apt-get install -y avahi-daemon
 # 2. Install Docker
-sudo apt instal -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` test"
-sudo apt update
-sudo apt install -y docker-ce
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo docker run hello-world
 # 3. Linux post-install add permissions
 sudo groupadd docker
