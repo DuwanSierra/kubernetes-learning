272d4905a06f93d9d0efdfe46b28c27f76f1a30f
Duwan Sierra <desierrag@gmail.com>
2022-05-21 16:03:57 -0500

    first commit

---
+++ install-dns-and-docker-ubuntu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#This base is take from repo https://gist.github.com/fredhsu/f3d927d765727181767b3b13a3a23704
+# 1. Install avahi-daemon
+sudo apt-get update -y
+sudo apt-get install -y avahi-daemon
+# 2. Install Docker
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo docker run hello-world
+# 3. Linux post-install add permissions
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+
+# 4. Install rancher in single node docker
+while getopts u:a:f: flag
+do
+    case "${flag}" in
+        r) rancher=${OPTARG};;
+    esac
+done
+echo "Install rancher: $rancher";
+if [ $rancher -eq  true ]
+then
+  docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher:latest
+fi
